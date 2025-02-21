cbca14a3f0b8369735101f59d13d2fee3a9362d1
niva009 <pranavpradeepe@gmail.com>
2025-02-21 12:01:19 +0530

    remove monngos error

--- config/config.js
+++ config/config.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const dbUrl = process.env.dbURI;
+const MONGO_URI = process.env.MONGO_URI;
+
+
+console.log("MONGO_URI:", process.env.MONGO_URI); 
+
 
 const connectDb = async() =>{
     try{
-        mongoose.connect(dbUrl,{
+        mongoose.connect("mongodb+srv://pranavpradeepe:i5ZxvHh96YWh6dVw@cluster0.exout.mongodb.net/rain_frops?retryWrites=true&w=majority&appName=Cluster0",{
         })
         console.log("db connection success")
     } catch(error){
